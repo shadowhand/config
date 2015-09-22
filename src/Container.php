ed4fa94aab3fa97796b67ec7382092fa7e33cac1
Gabriel Bull <me@gabrielbull.com>
2015-09-21 21:22:36 -0400

    Added polyfill for doctrine

---
+++ src/Container.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Sinergi\Config;
+
+use ReflectionClass;
+use Interop\Container\ContainerInterface;
+
+class Container implements ContainerInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $diContainer;
+
+    /**
+     * @var array
+     */
+    private $container = [];
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->diContainer = $container;
+    }
+
+    public function get($id)
+    {
+        $className = $this->getContainerValue($id);
+        if (is_callable($className)) {
+            return $className($this->diContainer, $id);
+        }
+        $class = new $className;
+        return $class($this->diContainer);
+    }
+
+    public function has($id)
+    {
+        return $this->getContainerValue($id) !== null;
+    }
+
+    protected function getContainerValue($id)
+    {
+        if (isset($this->container[$id])) {
+            return $this->container[$id];
+        }
+        foreach ($this->container as $alias => $concrete) {
+            $class = new ReflectionClass($id);
+            if (false === $class) {
+                return null;
+            }
+            do {
+                $name = $class->getName();
+                if ($alias == $name) {
+                    return $concrete;
+                }
+                $interfaces = $class->getInterfaceNames();
+                if (is_array($interfaces) && in_array($alias, $interfaces)) {
+                    return $concrete;
+                }
+                $class = $class->getParentClass();
+            } while (false !== $class);
+            return null;
+        }
+        return null;
+    }
+
+    public function add($alias, $className, $target = null)
+    {
+        if (null === $target) {
+            $target = $className;
+        } else {
+            $this->container[$className] = $target;
+        }
+        $this->container[$alias] = $target;
+    }
+}
---
+++ src/Polyfill/Doctrine/EntityManager.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Sinergi\Config\Polyfill\Doctrine;
+
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\ORM\Mapping\DefaultQuoteStrategy;
+use Doctrine\ORM\Tools\Setup;
+use Interop\Config\ConfigurationTrait;
+use Interop\Config\HasMandatoryOptions;
+use Interop\Config\HasContainerId;
+use Interop\Container\ContainerInterface;
+use Doctrine\ORM\EntityManager as DoctrineEntityManager;
+use Doctrine\DBAL\Driver\PDOMySql\Driver as PDOMySqlDriver;
+
+class EntityManager implements HasMandatoryOptions, HasContainerId
+{
+    use ConfigurationTrait;
+
+    public function __invoke(ContainerInterface $container)
+    {
+        $options = $this->options($container->get('config'));
+        $driverClass = $options['driverClass'];
+        $metadataDriverClass = $options['metadataDriverClass'];
+        $cacheDriverClass = isset($options['cacheDriverClass']) ? $options['cacheDriverClass'] : null;
+        $sqlLoggerDriverClass = $options['sqlLoggerDriverClass'];
+
+        $doctrineConfig = Setup::createConfiguration($options['is_dev_mode']);
+
+        $doctrineConfig->setMetadataDriverImpl(
+            new $metadataDriverClass(new AnnotationReader(), $options['paths'])
+        );
+
+        $doctrineConfig->setQuoteStrategy(new DefaultQuoteStrategy());
+
+        if ($cacheDriverClass) {
+            $cache = new $cacheDriverClass;
+            $doctrineConfig->setQueryCacheImpl($cache);
+            $doctrineConfig->setMetadataCacheImpl($cache);
+            $doctrineConfig->setHydrationCacheImpl($cache);
+            $doctrineConfig->setResultCacheImpl($cache);
+        }
+
+        if (isset($options['proxy_dir'])) {
+            $doctrineConfig->setProxyDir($options['proxy_dir']);
+            if (isset($connectionConfig['proxy_namespace'])) {
+                $doctrineConfig->setProxyNamespace($options['proxy_namespace']);
+            } else {
+                $doctrineConfig->setProxyNamespace('Proxies');
+            }
+        }
+
+        if (!isset($options['params']['charset'])) {
+            $options['params']['charset'] = 'utf8';
+        }
+
+        if (!isset($options['params']['driver'])) {
+            $options['params']['driver'] = (new $driverClass())->getName();
+        }
+
+        $entityManager = DoctrineEntityManager::create($options['params'], $doctrineConfig);
+
+        $connection = $entityManager->getConnection();
+
+        if ($driverClass === PDOMySqlDriver::class) {
+            $connection->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
+        }
+
+        if (isset($sqlLoggerDriverClass)) {
+            $connection->getConfiguration()->setSQLLogger(new $sqlLoggerDriverClass);
+        }
+
+        return $entityManager;
+    }
+
+    /**
+     * @return string
+     */
+    public function vendorName()
+    {
+        return 'doctrine';
+    }
+
+    /**
+     * @return string
+     */
+    public function packageName()
+    {
+        return 'connection';
+    }
+
+    /**
+     * @return string
+     */
+    public function containerId()
+    {
+        return 'default';
+    }
+
+    /**
+     * @return string[] List with mandatory options
+     */
+    public function mandatoryOptions()
+    {
+        return [
+            'driverClass',
+            'params',
+        ];
+    }
+
+    /**
+     * @return string[] List with optional options
+     */
+    public function optionalOptions()
+    {
+        return [
+            'metadataDriverClass',
+            'is_dev_mode',
+            'proxy_dir',
+            'proxy_namespace',
+            'paths',
+            'cacheDriverClass',
+            'sqlLoggerDriverClass',
+        ];
+    }
+}
